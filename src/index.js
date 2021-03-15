21c5a6a3f9eae681dfbf485a87c1f7d5f07dc131
Silvio Ramos <Silvio.Ramos@nic.uni.edu.ni>
2021-03-15 12:36:25 -0600

    msj listen server cambio.

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.delete('/items/:id', deleteItem);
 console.log(process.env.MYSQL_HOST);
 db.init()
     .then(() => {
-        app.listen(3000, () => console.log('Listening on port 3000'));
+        app.listen(3000, () => console.log('Listening on port 3000...'));
     })
     .catch(err => {
         console.error(err);
